7eec28df45f48e824917dee987ae0e42c5884f7a
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-11-17 02:49:25 -0500

    slightly clean up theremin.py. add some comments

--- theremin.py
+++ theremin.py
@@ -12,19 +12,28 @@ class Theremin:
 		self.freq = 550
 		self.volume = 0
 		self.thread = None
+		self.is_playing = False
 		self.event = None
 
+	# start playing a tone
 	def start(self):
-		self.event = threading.Event()
-		self.thread = threading.Thread(name="play_tone", target=play_tone, args=(self.stream,self.event,self))
-		self.thread.start()
+		if not self.is_playing:
+			self.is_playing = True
+			self.event = threading.Event()
+			self.thread = threading.Thread(name="play_tone", target=play_tone, args=(self.stream,self.event,self))
+			self.thread.start()
 		
+	# stop playing the tone
 	def stop(self):
 		self.event.set()
+		self.is_playing = False
 
+	# set the frequency of the tone
+	# works whether the tone is currently playing or not
 	def set_freq(self,freq):
 		self.freq = freq
 
+	# must call this before your application exits
 	def close(self):
 		self.stream.close()
 		self.p.terminate()
@@ -52,6 +61,11 @@ if __name__ == '__main__':
 	t.set_freq(470)
 	time.sleep(0.3)
 	t.set_freq(440)
+	time.sleep(1)
+	t.stop()
+	time.sleep(1)
+	t.start()
+	t.start()
 	time.sleep(1)
 	t.stop()
 	t.close()
