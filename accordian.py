8cd7ba7af2b771b66fd7952021b2b65bfb258363
Zachary Michaelov <zach.michaelov@gmail.com>
2013-11-16 21:56:30 -0500

    move accordian up a directory

--- accordian.py
+++ accordian.py
@@ -2,8 +2,8 @@
 import pygame
 
 pygame.mixer.init()
-in_chord = pygame.mixer.Sound("chord0.wav")
-out_chord = pygame.mixer.Sound("chord1.wav")
+in_chord = pygame.mixer.Sound("accordian/chord0.wav")
+out_chord = pygame.mixer.Sound("accordian/chord1.wav")
 
 def play(chord):
     if chord == "in":
