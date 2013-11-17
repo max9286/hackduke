543afe260159b8aff6c61a1f8151253044b05eb7
Zachary Michaelov <zach.michaelov@gmail.com>
2013-11-16 21:37:57 -0500

    accordian play() and stop() api complete

---
+++ accordian.py
@@ -0,0 +1,20 @@
+
+import pygame
+
+pygame.mixer.init()
+in_chord = pygame.mixer.Sound("chord1.wav")
+out_chord = pygame.mixer.Sound("chord2.wav")
+
+def play(chord):
+    if chord == "in":
+        in_chord.play()
+    elif chord == "out":
+        out_chord.play()
+
+    #while pygame.mixer.get_busy():
+    #    pygame.time.wait(20)
+
+
+def stop():
+    in_chord.stop()
+    out_chord.stop()
