3e269ee6e5413dcf0e98388aff6f74fbfb6cf18a
Zachary Michaelov <zach.michaelov@gmail.com>
2013-11-16 22:36:48 -0500

    added guitar interface

---
+++ guitar.py
@@ -0,0 +1,16 @@
+
+import pygame, os
+
+pygame.mixer.init()
+
+path = "sample_generator/instruments/guitar/"
+chords = []
+for i in xrange(5):
+    sound = pygame.mixer.Sound(os.path.join(path, 'chord%d.wav'%i))
+    chords.append(sound)
+def play(index):
+    chords[index].play()
+
+def stop():
+    for chord in chords:
+        chord.stop()
